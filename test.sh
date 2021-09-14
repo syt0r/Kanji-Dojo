f2c6e8dabc3d1b7faa72639b3cd90f46a271f14d
Yaroslav Shuliak <shuliak.yaroslav@gmail.com>
2021-09-14 14:45:54 +0300

    arg index fix

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 echo "{\"name\":\"Lint Check\",\"head_sha\":\"${GITHUB_SHA}\",\"conclusion\":\"success\"}"
-echo "token=[$0]"
-curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: Bearer $0" https://api.github.com/repos/SYtor/Kanji-Dojo/check-runs -d "{\"name\":\"Lint Check\",\"head_sha\":\"${GITHUB_SHA}\",\"conclusion\":\"success\"}"
+echo "token=[$1]"
+curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: Bearer $1" https://api.github.com/repos/SYtor/Kanji-Dojo/check-runs -d "{\"name\":\"Lint Check\",\"head_sha\":\"${GITHUB_SHA}\",\"conclusion\":\"success\"}"
