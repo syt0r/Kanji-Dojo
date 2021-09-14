c15593a3d33647a10d58a53df4c88bca83f17d80
Yaroslav Shuliak <shuliak.yaroslav@gmail.com>
2021-09-14 15:04:39 +0300

    xmllint test

--- test.sh
+++ test.sh
@@ -3,3 +3,5 @@ echo "token=[$1]"
 curl -X POST -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: Bearer $1" https://api.github.com/repos/SYtor/Kanji-Dojo/check-runs \
     -d "{\"name\":\"Lint Check\",\"head_sha\":\"${GITHUB_SHA}\",\"conclusion\":\"success\",\"output\":{\"title\":\"# Custom Title\",\"summary\":\"## Summary\",\"text\":\"### Text\"}}"
+
+xmllint
