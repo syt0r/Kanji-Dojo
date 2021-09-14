76ada23a3b677b95395f9b5160a519fef4772a78
Yaroslav Shuliak <shuliak.yaroslav@gmail.com>
2021-09-14 14:30:48 +0300

    token update

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 echo "{\"name\":\"Lint Check\",\"head_sha\":\"${GITHUB_SHA}\",\"conclusion\":\"success\"}"
-echo $GITHUB_TOKEN
-curl -X POST -H "Accept: application/vnd.github.v3+json" -H 'Authorization: Bearer ${{ GITHUB_TOKEN }}' https://api.github.com/repos/SYtor/Kanji-Dojo/check-runs -d "{\"name\":\"Lint Check\",\"head_sha\":\"${GITHUB_SHA}\",\"conclusion\":\"success\"}"
+echo "token=[{$github.token}]"
+curl -X POST -H "Accept: application/vnd.github.v3+json" -H 'Authorization: Bearer ${{ github.token }}' https://api.github.com/repos/SYtor/Kanji-Dojo/check-runs -d "{\"name\":\"Lint Check\",\"head_sha\":\"${GITHUB_SHA}\",\"conclusion\":\"success\"}"
