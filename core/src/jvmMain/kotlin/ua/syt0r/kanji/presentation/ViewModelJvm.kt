58fb84d7e5978b88b957c6c80d7f19edc994b4e7
syt0r <shuliak.yaroslav@gmail.com>
2023-03-17 13:36:25 +0200

    Persist state for multiplatform home navigation #16

--- core/src/jvmMain/kotlin/ua/syt0r/kanji/presentation/ViewModelJvm.kt
+++ core/src/jvmMain/kotlin/ua/syt0r/kanji/presentation/ViewModelJvm.kt
@@ -1,13 +1,17 @@
 package ua.syt0r.kanji.presentation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import org.koin.core.parameter.parametersOf
 import org.koin.java.KoinJavaComponent.getKoin
 
 @Composable
 actual inline fun <reified T> getMultiplatformViewModel(): T {
-    val coroutineScope = rememberCoroutineScope()
-    return rememberSaveable { getKoin().get { parametersOf(coroutineScope) } }
+    /***
+     * Using custom coroutine scope instead of remember one since it can leave composition when
+     * navigating so view model will have canceled scope after returning to the screen
+     */
+    return rememberSaveable { getKoin().get { parametersOf(CoroutineScope(Dispatchers.Unconfined)) } }
 }
--- core/src/jvmMain/kotlin/ua/syt0r/kanji/presentation/screen/main/screen/home/HomeNavigationJvm.kt
+++ core/src/jvmMain/kotlin/ua/syt0r/kanji/presentation/screen/main/screen/home/HomeNavigationJvm.kt
@@ -4,13 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import ua.syt0r.kanji.presentation.getMultiplatformViewModel
 import ua.syt0r.kanji.presentation.screen.main.MainNavigationState
 import ua.syt0r.kanji.presentation.screen.main.screen.home.data.HomeScreenTab
 import ua.syt0r.kanji.presentation.screen.main.screen.home.screen.practice_dashboard.PracticeDashboardScreen
-import ua.syt0r.kanji.presentation.screen.main.screen.home.screen.practice_dashboard.PracticeDashboardScreenContract
 import ua.syt0r.kanji.presentation.screen.main.screen.home.screen.search.SearchScreen
-import ua.syt0r.kanji.presentation.screen.main.screen.home.screen.search.SearchScreenContract
 import ua.syt0r.kanji.presentation.screen.main.screen.home.screen.settings.SettingsScreen
 
 @Composable
@@ -33,22 +32,35 @@ actual fun HomeNavigationContent(
     mainNavigationState: MainNavigationState
 ) {
     homeNavigationState as MultiplatformHomeNavigationState
-    when (homeNavigationState.selectedTab.value) {
-        HomeScreenTab.PRACTICE_DASHBOARD -> {
-            val viewModel = getMultiplatformViewModel<PracticeDashboardScreenContract.ViewModel>()
-            PracticeDashboardScreen(mainNavigationState, viewModel)
-        }
 
-        HomeScreenTab.SEARCH -> {
-            val viewModel = getMultiplatformViewModel<SearchScreenContract.ViewModel>()
-            SearchScreen(mainNavigationState, viewModel)
-        }
+    val stateHolder = rememberSaveableStateHolder()
 
-        HomeScreenTab.SETTINGS -> {
-            SettingsScreen(
-                viewModel = getMultiplatformViewModel(),
-                mainNavigationState = mainNavigationState
-            )
+    val tab = homeNavigationState.selectedTab.value
+
+    stateHolder.SaveableStateProvider(tab.name) {
+        when (tab) {
+            HomeScreenTab.PRACTICE_DASHBOARD -> {
+                PracticeDashboardScreen(
+                    mainNavigationState = mainNavigationState,
+                    viewModel = getMultiplatformViewModel()
+                )
+            }
+
+            HomeScreenTab.SEARCH -> {
+                SearchScreen(
+                    mainNavigationState = mainNavigationState,
+                    viewModel = getMultiplatformViewModel()
+                )
+            }
+
+            HomeScreenTab.SETTINGS -> {
+                SettingsScreen(
+                    viewModel = getMultiplatformViewModel(),
+                    mainNavigationState = mainNavigationState
+                )
+            }
         }
     }
+
+
 }
