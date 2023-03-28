cdd874fcdae5441857cff6b93a79406146d84c46
syt0r <shuliak.yaroslav@gmail.com>
2023-03-28 10:58:56 +0300

    Update version to 1.3

--- buildSrc/src/main/kotlin/AppVerion.kt
+++ buildSrc/src/main/kotlin/AppVerion.kt
@@ -1,8 +1,8 @@
 object AppVerion {
 
-    const val versionCode = 12
+    const val versionCode = 13
 
-    const val versionName = "1.2"
+    const val versionName = "1.3"
 
     // Requires 3 numbers
     const val desktopAppVersion = "$versionName.0"
