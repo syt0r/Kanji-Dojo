fd4df51617c85add9af4b2ac1fe5ba93e55151a8
syt0r <shuliak.yaroslav@gmail.com>
2024-01-23 12:48:25 +0200

    Update version code to 18

--- buildSrc/src/main/kotlin/AppVersion.kt
+++ buildSrc/src/main/kotlin/AppVersion.kt
@@ -1,8 +1,8 @@
 object AppVersion {
 
-    const val versionCode = 17
+    const val versionCode = 18
 
-    const val versionName = "1.7"
+    const val versionName = "1.8"
 
     // Requires 3 numbers
     const val desktopAppVersion = "$versionName.0"
