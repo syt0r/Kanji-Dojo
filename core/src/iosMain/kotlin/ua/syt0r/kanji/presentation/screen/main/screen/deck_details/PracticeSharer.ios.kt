427eb72fddd0924a07aa15042632fac62d7dcddf
syt0r <shuliak.yaroslav@gmail.com>
2025-04-09 17:07:30 +0300

    Letter deck sharing iOS support

--- core/src/iosMain/kotlin/ua/syt0r/kanji/presentation/screen/main/screen/deck_details/PracticeSharer.ios.kt
+++ core/src/iosMain/kotlin/ua/syt0r/kanji/presentation/screen/main/screen/deck_details/PracticeSharer.ios.kt
@@ -3,6 +3,8 @@ package ua.syt0r.kanji.presentation.screen.main.screen.deck_details
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import platform.UIKit.UIActivityViewController
+import platform.UIKit.UIApplication
 
 @Composable
 actual fun rememberPracticeSharer(snackbarHostState: SnackbarHostState): PracticeSharer {
@@ -10,7 +12,18 @@ actual fun rememberPracticeSharer(snackbarHostState: SnackbarHostState): Practic
 }
 
 class IosPracticeSharer : PracticeSharer {
+
     override fun share(data: String) {
-        TODO("Not yet implemented")
+        val activityViewController = UIActivityViewController(
+            activityItems = listOf(data),
+            applicationActivities = null
+        )
+
+        UIApplication.sharedApplication.keyWindow!!.rootViewController!!.presentViewController(
+            activityViewController,
+            animated = true,
+            completion = null
+        )
     }
+
 }
